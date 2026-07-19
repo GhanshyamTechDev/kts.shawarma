8ff6a66fb14261b5f64e8209872d28afd29b0258
Ghanshyam0007 <ghanshyamdadange@gmail.com>
2026-07-19 20:01:07 +0530

    updated for js locater

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
+import setupLocatorUI from '@locator/runtime'
+
+if (import.meta.env.DEV) {
+  setupLocatorUI()
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import babel from '@rolldown/plugin-babel'
 
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => ({
+  plugins: [
+    react(),
+    command === 'serve' && babel({
+      plugins: [['@locator/babel-jsx/dist', { dataAttribute: 'path' }]],
+    }),
+  ],
+}));
